fc617d1e53108a13cd0db5bcd2513c1601206b08
Anastasiia <104034931+anastizs@users.noreply.github.com>
2023-10-22 16:39:24 +0300

    swiper

---
+++ js/slider-feedback.js
@@ -0,0 +1,35 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoHeight: false,
+  centeredSlides: true,
+  slidesPerView: 1,
+  // Responsive breakpoints
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  /*scrollbar: {
+    el: '.swiper-scrollbar',
+  },*/
+});
